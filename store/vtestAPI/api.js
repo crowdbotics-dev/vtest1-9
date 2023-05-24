6c702f7fece539953ac2451312a47cd9fc701f08
vinay sharma <vinay.sharma@crowdbotics.com>
2023-05-24 23:24:18 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/vtestAPI/api.js
+++ store/vtestAPI/api.js
@@ -1,122 +1,168 @@
-import axios from "axios"
+import axios from "axios";
 const vtestAPI = axios.create({
   baseURL: "https://vtest1-9.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_home_list(payload) {
-  return vtestAPI.get(`/api/v1/home/`)
+  return vtestAPI.get(`/api/v1/home/`);
 }
+
 function api_v1_home_create(payload) {
-  return vtestAPI.post(`/api/v1/home/`, payload.data)
+  return vtestAPI.post(`/api/v1/home/`, payload.data);
 }
+
 function api_v1_user_list(payload) {
-  return vtestAPI.get(`/api/v1/user/`)
+  return vtestAPI.get(`/api/v1/user/`);
 }
+
 function api_v1_user_create(payload) {
-  return vtestAPI.post(`/api/v1/user/`, payload.data)
+  return vtestAPI.post(`/api/v1/user/`, payload.data);
 }
+
 function api_v1_login_create(payload) {
-  return vtestAPI.post(`/api/v1/login/`, payload.data)
+  return vtestAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return vtestAPI.post(`/api/v1/signup/`, payload.data)
+  return vtestAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return vtestAPI.get(`/rest-auth/user/`)
+  return vtestAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return vtestAPI.put(`/rest-auth/user/`, payload.data)
+  return vtestAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return vtestAPI.patch(`/rest-auth/user/`, payload.data)
+  return vtestAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
-  return vtestAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
+  return vtestAPI.get(`/api-docs/schema/`, {
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_meetupss_list(payload) {
-  return vtestAPI.get(`/api/v1/meetupss/`)
+  return vtestAPI.get(`/api/v1/meetupss/`);
 }
+
 function api_v1_meetupss_create(payload) {
-  return vtestAPI.post(`/api/v1/meetupss/`, payload.data)
+  return vtestAPI.post(`/api/v1/meetupss/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return vtestAPI.post(`/rest-auth/login/`, payload.data)
+  return vtestAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function api_v1_home_retrieve(payload) {
-  return vtestAPI.get(`/api/v1/home/${payload.id}/`)
+  return vtestAPI.get(`/api/v1/home/${payload.id}/`);
 }
+
 function api_v1_home_update(payload) {
-  return vtestAPI.put(`/api/v1/home/${payload.id}/`, payload.data)
+  return vtestAPI.put(`/api/v1/home/${payload.id}/`, payload.data);
 }
+
 function api_v1_home_partial_update(payload) {
-  return vtestAPI.patch(`/api/v1/home/${payload.id}/`, payload.data)
+  return vtestAPI.patch(`/api/v1/home/${payload.id}/`, payload.data);
 }
+
 function api_v1_home_destroy(payload) {
-  return vtestAPI.delete(`/api/v1/home/${payload.id}/`)
+  return vtestAPI.delete(`/api/v1/home/${payload.id}/`);
 }
+
 function api_v1_main_menu_list(payload) {
-  return vtestAPI.get(`/api/v1/main_menu/`)
+  return vtestAPI.get(`/api/v1/main_menu/`);
 }
+
 function api_v1_main_menu_create(payload) {
-  return vtestAPI.post(`/api/v1/main_menu/`, payload.data)
+  return vtestAPI.post(`/api/v1/main_menu/`, payload.data);
 }
+
 function api_v1_user_retrieve(payload) {
-  return vtestAPI.get(`/api/v1/user/${payload.id}/`)
+  return vtestAPI.get(`/api/v1/user/${payload.id}/`);
 }
+
 function api_v1_user_update(payload) {
-  return vtestAPI.put(`/api/v1/user/${payload.id}/`, payload.data)
+  return vtestAPI.put(`/api/v1/user/${payload.id}/`, payload.data);
 }
+
 function api_v1_user_partial_update(payload) {
-  return vtestAPI.patch(`/api/v1/user/${payload.id}/`, payload.data)
+  return vtestAPI.patch(`/api/v1/user/${payload.id}/`, payload.data);
 }
+
 function api_v1_user_destroy(payload) {
-  return vtestAPI.delete(`/api/v1/user/${payload.id}/`)
+  return vtestAPI.delete(`/api/v1/user/${payload.id}/`);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return vtestAPI.get(`/rest-auth/logout/`)
+  return vtestAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return vtestAPI.post(`/rest-auth/logout/`)
+  return vtestAPI.post(`/rest-auth/logout/`);
 }
+
 function api_v1_meetupss_retrieve(payload) {
-  return vtestAPI.get(`/api/v1/meetupss/${payload.id}/`)
+  return vtestAPI.get(`/api/v1/meetupss/${payload.id}/`);
 }
+
 function api_v1_meetupss_update(payload) {
-  return vtestAPI.put(`/api/v1/meetupss/${payload.id}/`, payload.data)
+  return vtestAPI.put(`/api/v1/meetupss/${payload.id}/`, payload.data);
 }
+
 function api_v1_meetupss_partial_update(payload) {
-  return vtestAPI.patch(`/api/v1/meetupss/${payload.id}/`, payload.data)
+  return vtestAPI.patch(`/api/v1/meetupss/${payload.id}/`, payload.data);
 }
+
 function api_v1_meetupss_destroy(payload) {
-  return vtestAPI.delete(`/api/v1/meetupss/${payload.id}/`)
+  return vtestAPI.delete(`/api/v1/meetupss/${payload.id}/`);
 }
+
 function api_v1_main_menu_retrieve(payload) {
-  return vtestAPI.get(`/api/v1/main_menu/${payload.id}/`)
+  return vtestAPI.get(`/api/v1/main_menu/${payload.id}/`);
 }
+
 function api_v1_main_menu_update(payload) {
-  return vtestAPI.put(`/api/v1/main_menu/${payload.id}/`, payload.data)
+  return vtestAPI.put(`/api/v1/main_menu/${payload.id}/`, payload.data);
 }
+
 function api_v1_main_menu_partial_update(payload) {
-  return vtestAPI.patch(`/api/v1/main_menu/${payload.id}/`, payload.data)
+  return vtestAPI.patch(`/api/v1/main_menu/${payload.id}/`, payload.data);
 }
+
 function api_v1_main_menu_destroy(payload) {
-  return vtestAPI.delete(`/api/v1/main_menu/${payload.id}/`)
+  return vtestAPI.delete(`/api/v1/main_menu/${payload.id}/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return vtestAPI.post(`/rest-auth/registration/`, payload.data)
+  return vtestAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return vtestAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return vtestAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return vtestAPI.post(`/rest-auth/password/change/`, payload.data)
+  return vtestAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return vtestAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return vtestAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return vtestAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return vtestAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_home_list,
   api_v1_home_create,
@@ -156,4 +202,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
--- store/vtestAPI/meetupsses.slice.js
+++ store/vtestAPI/meetupsses.slice.js
@@ -1,48 +1,36 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
-import { apiService } from "./api"
-export const api_v1_meetupss_list = createAsyncThunk(
-  "meetupsses/api_v1_meetupss_list",
-  async payload => {
-    const response = await apiService.api_v1_meetupss_list(payload)
-    return response.data
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { apiService } from "./api";
+export const api_v1_meetupss_list = createAsyncThunk("meetupsses/api_v1_meetupss_list", async payload => {
+  const response = await apiService.api_v1_meetupss_list(payload);
+  return response.data;
+});
+export const api_v1_meetupss_create = createAsyncThunk("meetupsses/api_v1_meetupss_create", async payload => {
+  const response = await apiService.api_v1_meetupss_create(payload);
+  return response.data;
+});
+export const api_v1_meetupss_retrieve = createAsyncThunk("meetupsses/api_v1_meetupss_retrieve", async payload => {
+  const response = await apiService.api_v1_meetupss_retrieve(payload);
+  return response.data;
+});
+export const api_v1_meetupss_update = createAsyncThunk("meetupsses/api_v1_meetupss_update", async payload => {
+  const response = await apiService.api_v1_meetupss_update(payload);
+  return response.data;
+});
+export const api_v1_meetupss_partial_update = createAsyncThunk("meetupsses/api_v1_meetupss_partial_update", async payload => {
+  const response = await apiService.api_v1_meetupss_partial_update(payload);
+  return response.data;
+});
+export const api_v1_meetupss_destroy = createAsyncThunk("meetupsses/api_v1_meetupss_destroy", async payload => {
+  const response = await apiService.api_v1_meetupss_destroy(payload);
+  return response.data;
+});
+const initialState = {
+  entities: [],
+  api: {
+    loading: "idle",
+    error: null
   }
-)
-export const api_v1_meetupss_create = createAsyncThunk(
-  "meetupsses/api_v1_meetupss_create",
-  async payload => {
-    const response = await apiService.api_v1_meetupss_create(payload)
-    return response.data
-  }
-)
-export const api_v1_meetupss_retrieve = createAsyncThunk(
-  "meetupsses/api_v1_meetupss_retrieve",
-  async payload => {
-    const response = await apiService.api_v1_meetupss_retrieve(payload)
-    return response.data
-  }
-)
-export const api_v1_meetupss_update = createAsyncThunk(
-  "meetupsses/api_v1_meetupss_update",
-  async payload => {
-    const response = await apiService.api_v1_meetupss_update(payload)
-    return response.data
-  }
-)
-export const api_v1_meetupss_partial_update = createAsyncThunk(
-  "meetupsses/api_v1_meetupss_partial_update",
-  async payload => {
-    const response = await apiService.api_v1_meetupss_partial_update(payload)
-    return response.data
-  }
-)
-export const api_v1_meetupss_destroy = createAsyncThunk(
-  "meetupsses/api_v1_meetupss_destroy",
-  async payload => {
-    const response = await apiService.api_v1_meetupss_destroy(payload)
-    return response.data
-  }
-)
-const initialState = { entities: [], api: { loading: "idle", error: null } }
+};
 const meetupssesSlice = createSlice({
   name: "meetupsses",
   initialState,
@@ -50,117 +38,108 @@ const meetupssesSlice = createSlice({
   extraReducers: {
     [api_v1_meetupss_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_meetupss_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = action.payload
-        state.api.loading = "idle"
+        state.entities = action.payload;
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_meetupss_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities.push(action.payload)
-        state.api.loading = "idle"
+        state.entities.push(action.payload);
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_retrieve.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_meetupss_retrieve.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = [
-          ...state.entities.filter(record => record.id !== action.payload.id),
-          action.payload
-        ]
-        state.api.loading = "idle"
+        state.entities = [...state.entities.filter(record => record.id !== action.payload.id), action.payload];
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_retrieve.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_update.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_meetupss_update.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record =>
-          record.id === action.payload.id ? action.payload : record
-        )
-        state.api.loading = "idle"
+        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_update.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_partial_update.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_meetupss_partial_update.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record =>
-          record.id === action.payload.id ? action.payload : record
-        )
-        state.api.loading = "idle"
+        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_partial_update.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_destroy.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending"
+        state.api.loading = "pending";
       }
     },
     [api_v1_meetupss_destroy.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = state.entities.filter(
-          record => record.id !== action.meta.arg?.id
-        )
-        state.api.loading = "idle"
+        state.entities = state.entities.filter(record => record.id !== action.meta.arg?.id);
+        state.api.loading = "idle";
       }
     },
     [api_v1_meetupss_destroy.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error
-        state.api.loading = "idle"
+        state.api.error = action.error;
+        state.api.loading = "idle";
       }
     }
   }
-})
+});
 export default {
   api_v1_meetupss_list,
   api_v1_meetupss_create,
@@ -169,4 +148,4 @@ export default {
   api_v1_meetupss_partial_update,
   api_v1_meetupss_destroy,
   slice: meetupssesSlice
-}
+};
