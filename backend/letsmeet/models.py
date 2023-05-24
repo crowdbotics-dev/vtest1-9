a4487c9c8ca7b8e2f41fb344ad330708dc45fcf8
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-24 15:23:51 +0000

    Update models

--- backend/letsmeet/models.py
+++ backend/letsmeet/models.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from django.db import models
 class Meetupss(models.Model):
     'Generated Model'
-    contact = models.IntegerField(max_length=256,null=True,blank=True,)
+    contacts = models.IntegerField(null=True,blank=True,max_length=256,)
 
 # Create your models here.
